99027b17d9cb27cf7f5fa91d80b95759af1254b5
Ljaewon-123 <jml6388@naver.com>
2022-01-04 00:01:36 +0900

    Decimal

---
+++ Decimal_Converter.py
@@ -0,0 +1,60 @@
+# a = divmod(147,2)
+# print(a[0])
+# print(a[1])
+# print(a)
+# b = a[0]
+# print(b)
+
+# B = [list(map(int, x)) for x in a]      ##### !! 중요 문자열 -> int 로 반환
+
+b =[]*8
+
+def divide(num):
+    while(True):
+        a = divmod(num, 2)
+
+        while (num > 1):
+            # print(a)
+            # a1 = a[1]
+            # a0 = a[0]
+            # print(a1)
+            # print(a0)
+            if 0 in a:
+                print(0, end = "")
+                b.append(0)
+                num = a[0]
+                break
+            elif 1 in a:
+                print(1, end="")
+                num = a[0]
+                b.append(1)
+                break
+        if 0 == num:
+            print(0, end="")
+            b.append(0)
+            break
+        elif 1 == num:
+            print(1, end="")
+            b.append(1)
+            break
+    print()
+    string = "".join([str(_) for _ in b])
+    print(string[::-1])     # 뒤집어줌  현재 string는 문자열 상태
+    return b    # 무엇을 리턴해야 할까?
+
+
+a = divide(286)
+
+print(a)          # 그냥 return 을 바꿔서 한번에
+
+
+for i in range(len(a)):
+    if a[i] == 0:
+        pass
+    else:
+       pass
+
+
+
+
+
