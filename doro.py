9bcc5e8326ee3158e611fbc2c08465e04fc94b73
Ljaewon-123 <jml6388@naver.com>
2022-03-20 02:26:06 +0900

    ysg

--- doro.py
+++ doro.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from xml.etree import ElementTree
 import requests
 import json
@@ -26,32 +27,102 @@ SiDo = {'서울특별시':['11',{'강남구':'680','강동구':'740','강북구'
 # print(SiDo['서울특별시'][1]['강남구'])
 # print(SiDo['서울특별시'][1].keys())
 
+Year = ['2017','2018','2019','2020']
+sido_list = ['서울특별시','부산광역시']
+
+print(SiDo[sido_list[0]][1].values())
+
 service_key='SaSnehC34rO3z%2Ff%2Fjavc%2FfzjQCJwxfuTfLP5JNBfIOGmvKQtXfuAX8tm2GGi1%2FY2lX3Gbx07wScwmZsCdeLpyQ%3D%3D'
+for year in Year:
+        for sido in sido_list:
+                print(sido)
+                for gugun in SiDo[sido][1].values():
+                        print(gugun)
+        # url = f'http://apis.data.go.kr/B552061/frequentzoneLg/getRestFrequentzoneLg?serviceKey={service_key}&searchYearCd={year}&siDo={SiDo[sido_list[0]][0]}&guGun={SiDo[sido_list[0]][1]["강남구"]}&type=json&numOfRows=9999&pageNo=1'
+                        url = f'http://apis.data.go.kr/B552061/frequentzoneLg/getRestFrequentzoneLg?serviceKey={service_key}&searchYearCd={year}&siDo={SiDo[sido_list[0]][0]}&guGun={gugun}&type=json&numOfRows=9999&pageNo=1'
+                        print(url)
 
-url = f'http://apis.data.go.kr/B552061/frequentzoneLg/getRestFrequentzoneLg?serviceKey={service_key}&searchYearCd=2020&siDo={SiDo["서울특별시"][0]}&guGun={SiDo["서울특별시"][1]["강남구"]}&type=json&numOfRows=9999&pageNo=1'
-print(url)
+                        # format123 = 'asdf{0},{1},{2}'
+                        # ff123 = format123.format(123,421,56)
+                        # print(ff123)
 
-resp = requests.get(url)
-json = resp.json()
-print(json)
-# tree = ElementTree.fromstring(resp.text)
-# print(tree)
-'''
-5 : 지점명  spot_nm
-6 : 발생건수  occrrnc_cnt
-7 : 사상자수  caslt_cnt
-8 : 사망자수  dth_dnv_cnt
-9 : 중상자수  se_dnv_cnt
-10 : 경상자수  sl_dnv_cnt
-12 : 폴리곤  geom_json
-13 : 경도   lo_crd
-14 : 위도   la_crd
+                        resp = requests.get(url)
+                        # json
+                        json = resp.json()
+                        # print(json)
+                        # xml
+                        # tree = ElementTree.fromstring(resp.text)
+                        # print(tree)
+                        '''
+                        5 : 지점명  spot_nm
+                        6 : 발생건수  occrrnc_cnt
+                        7 : 사상자수  caslt_cnt
+                        8 : 사망자수  dth_dnv_cnt
+                        9 : 중상자수  se_dnv_cnt
+                        10 : 경상자수  sl_dnv_cnt
+                        12 : 폴리곤  geom_json
+                        13 : 경도   lo_crd
+                        14 : 위도   la_crd
+                        
+                        '''
+                        # print(json['items']['item'])
+                        data = json['items']['item']
+                        # print(data)
+                        lst = []
+                        total_occrrnc_cnt ,total_caslt_cnt, total_dth_dnv_cnt,total_se_dnv_cnt ,total_sl_dnv_cnt = 0,0,0,0,0
 
-'''
-# print(json['items']['item'])
-data = json['items']['item']
+                        for item in data:
+                                print(item)
+                                tmp = {}
+                                for key in item.keys():
+                                        if key == 'spot_nm':
+                                                tmp['상세주소'] = item[key]
+                                        elif key == 'occrrnc_cnt':
+                                                tmp['발생건수'] = item[key]
+                                                total_occrrnc_cnt += item[key]
+                                        elif key == 'caslt_cnt':
+                                                tmp['사상자수'] = item[key]
+                                                total_caslt_cnt += item[key]
+                                        elif key == 'dth_dnv_cnt':
+                                                tmp['사망자수'] = item[key]
+                                                total_dth_dnv_cnt += item[key]
+                                        elif key == 'se_dnv_cnt':
+                                                tmp['중상자수'] = item[key]
+                                                total_se_dnv_cnt = +item[key]
+                                        elif key == 'sl_dnv_cnt':
+                                                tmp['경상자수'] = item[key]
+                                                total_sl_dnv_cnt = +item[key]
+                                        elif key == 'geom_json':
+                                                tmp['폴리곤'] = item[key]
+                                        elif key == 'lo_crd':
+                                                tmp['경도'] = item[key]
+                                        elif key == 'la_crd':
+                                                tmp['위도'] = item[key]
+                                lst.append(tmp)
+
+                        # 데이터 없으면 에러 발생
+                        try:
+                                tmp['총합발생건수'] = total_occrrnc_cnt
+                                tmp['총합사상자수'] = total_caslt_cnt
+                                tmp['총합사망자수'] = total_dth_dnv_cnt
+                                tmp['총합중상자수'] = total_se_dnv_cnt
+                                tmp['총합경상자수'] = total_sl_dnv_cnt
+                                lst.append(tmp)
+                        except NameError as e:
+                                print('데이터가 없음')
+
+                        print(lst)
 
 
+
+
+# a = {'s':['11',{'asdf':'123',"zxcv":'568'}]}
+# print(a['s'][1].values())
+
+# 유형== 파일이름?? 밑에 시도 밑에 년도 밑에 구별 데이터
+# 파일 명을 유형별로?? 할꺼면 파일명을 리스트에 안넣어도됨
+# class 에서 url 를 매개변수로 받는 형식 문제 : 파일 저장 으캄
+# 매개변수로 url과 파일 명을 같이 받음
 
 
 # xml
