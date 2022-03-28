c17009e081ee495f9bfe953838f44bb5a8f13e22
Ljaewon-123 <jml6388@naver.com>
2022-03-29 00:07:03 +0900

    fix

--- accident/accident/models.py
+++ accident/accident/models.py
@@ -8,6 +8,188 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class FrequentzonechildAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'frequentzonechild_accident'
+
+
+class FrequentzonelgAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'frequentzonelg_accident'
+
+
+class FrequentzoneoldmanAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'frequentzoneoldman_accident'
+
+
+class FrequentzonetmzonAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'frequentzonetmzon_accident'
+
+
 class InfCarAcc(models.Model):
     id = models.IntegerField(primary_key=True)
     cg = models.TextField(db_column='CG', blank=True, null=True)  # Field name made lowercase.
@@ -130,6 +312,40 @@ class InfYellowcarpet(models.Model):
         db_table = 'inf_yellowcarpet'
 
 
+class JaywalkingAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'jaywalking_accident'
+
+
+class SchoolzonechildAccident(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.TextField(blank=True, null=True)
+    occur = models.IntegerField(blank=True, null=True)
+    death_injury = models.IntegerField(blank=True, null=True)
+    death = models.IntegerField(blank=True, null=True)
+    s_injury = models.IntegerField(blank=True, null=True)
+    l_injury = models.IntegerField(blank=True, null=True)
+    lot = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'schoolzonechild_accident'
+
+
 class Year(models.Model):
     id = models.IntegerField(primary_key=True)
     year = models.IntegerField(blank=True, null=True)
@@ -137,3 +353,5 @@ class Year(models.Model):
     class Meta:
         managed = False
         db_table = 'year'
+
+
