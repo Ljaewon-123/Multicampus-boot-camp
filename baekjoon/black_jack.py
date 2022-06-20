0e297083570286b66964c665561fcb6e17188b8d
Ljaewon-123 <jml6388@naver.com>
2022-06-21 00:48:02 +0900

    black_jack

---
+++ baekjoon/black_jack.py
@@ -0,0 +1,63 @@
+# 첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다. 넘지 않는다 == 초과 라고하면 M 이 21이면 21까지는 됨
+# 중복불가란말 없으니 그냥 한다
+# 방법은 그려지는데 구현에서 애먹음
+# 3번이 쭈르륵 그다음 2번이 한칸식 움직이면서 3번 주르륵 마지막은 2,3번 끝에 있고 1번이 주르륵
+
+from random import *
+
+while True:
+    a= int(input('첫번째수:'))
+    b= int(input('두번재수:'))
+    # a = 10
+    # b = 100
+
+    if not a>=3 and a <= 100:
+        continue
+    elif not b >=10 and b <= 300000:
+        continue
+    else:
+        break
+
+lst = []
+sum1 = []
+for i in range(a):
+    # print(randint(3, b))
+    num = randint(3,b)
+    lst.append(num)
+
+print(lst)
+cnt = 0
+i = 0
+while i < len(lst):
+
+    if cnt == a-2:
+        sum1.append(lst[i] + lst[-2] + lst[-1])
+        i += 1
+        if i == a-3:
+            break
+
+    else:
+        sum1.append(lst[0]+lst[1+cnt]+lst[i+2])
+        i += 1
+        if a-3 == i:  # 마지막에서 두번재를 가르키면 cnt가 커지면서 2번이 움직이게됨
+            sum1.append(lst[0] + lst[1+cnt] + lst[i + 2])
+            cnt += 1
+            # print(cnt)
+            i = 0
+
+            continue
+
+print(sum1)
+
+while True:
+    # a = max(sum1)
+    # print(a)
+    if not sum1:
+        print('안되니까 다시 돌려 귀찮아서 while문 다시 안씀 ')
+        break
+    if max(sum1) > b:
+        sum1.remove(max(sum1)) # ValueError: max() arg is an empty sequence ## 전부 삭제되어서 비어있는거
+        continue
+    elif max(sum1) <= b:
+        print(max(sum1))
+        break
--- pre.py
+++ pre.py
@@ -3,8 +3,18 @@ import requests
 
 url = 'http://apis.data.go.kr/1520635/OceanBiospeciesInfoService/getOceanBiospeciesResult?' \
       'serviceKey=SaSnehC34rO3z%2Ff%2Fjavc%2FfzjQCJwxfuTfLP5JNBfIOGmvKQtXfuAX8tm2GGi1%2FY2lX3Gbx07wScwmZsCdeLpyQ%3D%3D&' \
-      'numOfRows=1000000000&pageNo=1'
+      'numOfRows=100000&pageNo=1'
+
+# # # 해양수산부 조위 실측 실측치가 있다!
+# url = 'http://www.khoa.go.kr/api/oceangrid/tideObs/search.do?ServiceKey=CndQ9ayWwjk5aH/aT22Bzw==&ObsCode=DT_0002&Date=20220412&ResultType=json'
+# #
+# # # http://www.khoa.go.kr/api/oceangrid/tideObsPre/search.do?ServiceKey=인증키&ObsCode=관측소 번호&Date=검색 기준 날짜&ResultType=json
+# # # 예측
+# url2 = 'http://www.khoa.go.kr/api/oceangrid/tideObsPre/search.do?ServiceKey=CndQ9ayWwjk5aH/aT22Bzw==&ObsCode=DT_0002&Date=20190412&ResultType=json'
+
+
 print(url)
+# print(url2)
 
 resp = requests.get(url)
 print(resp)
