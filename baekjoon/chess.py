414de95cf9701cf7088dc789b82cbb52ac389cca
Ljaewon-123 <jml6388@naver.com>
2022-06-24 16:01:39 +0900

    chess

---
+++ baekjoon/chess.py
@@ -0,0 +1,30 @@
+# ㅋㅋ;;
+N, M = map(int, input().split())
+origin = []
+count = []
+
+for _ in range(N):
+    origin.append(input())
+
+print(origin)
+# 각 행 구분을 해줘야함
+
+for a in range(N-7):
+    for b in range(M-7):
+        index1 = 0
+        index2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if origin[i][j] != 'W':
+                        index1 += 1
+                    if origin[i][j] != 'B':
+                        index2 += 1
+                else:
+                    if origin[i][j] != 'B':
+                        index1 += 1
+                    if origin[i][j] != 'W':
+                        index2 += 1
+        count.append(min(index1, index2))
+
+print(min(count))
