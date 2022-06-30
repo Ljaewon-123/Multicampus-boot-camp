94df843fec9f65cf6a8ada2d4853c0f1b6d18858
Ljaewon-123 <jml6388@naver.com>
2022-06-30 20:38:45 +0900

    maze

--- pre.py
+++ pre.py
@@ -1,20 +1,13 @@
-import folium
-import requests
+nameAry = ['블','레','마마','에이','걸스','트와이스']
+imim = nameAry[2]
+# 문자지만 비교가 가능하다
+for i in nameAry:
+      print(i,end=' ')
 
-url = 'http://apis.data.go.kr/1520635/OceanBiospeciesInfoService/getOceanBiospeciesResult?' \
-      'serviceKey=SaSnehC34rO3z%2Ff%2Fjavc%2FfzjQCJwxfuTfLP5JNBfIOGmvKQtXfuAX8tm2GGi1%2FY2lX3Gbx07wScwmZsCdeLpyQ%3D%3D&' \
-      'numOfRows=100000&pageNo=1'
+      if i > imim:
+            print('small')
+      elif i < imim:
+            print('big')
+      else:
+            print('asdfasdf')
 
-# # # 해양수산부 조위 실측 실측치가 있다!
-# url = 'http://www.khoa.go.kr/api/oceangrid/tideObs/search.do?ServiceKey=CndQ9ayWwjk5aH/aT22Bzw==&ObsCode=DT_0002&Date=20220412&ResultType=json'
-# #
-# # # http://www.khoa.go.kr/api/oceangrid/tideObsPre/search.do?ServiceKey=인증키&ObsCode=관측소 번호&Date=검색 기준 날짜&ResultType=json
-# # # 예측
-# url2 = 'http://www.khoa.go.kr/api/oceangrid/tideObsPre/search.do?ServiceKey=CndQ9ayWwjk5aH/aT22Bzw==&ObsCode=DT_0002&Date=20190412&ResultType=json'
-
-
-print(url)
-# print(url2)
-
-resp = requests.get(url)
-print(resp)
