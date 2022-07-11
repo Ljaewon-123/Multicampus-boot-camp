ee20ec4b99e6671d3807b24875d5886c078490df
Ljaewon-123 <jml6388@naver.com>
2022-07-11 16:14:18 +0900

    bestdque

--- coding/프린터.py
+++ coding/프린터.py
@@ -20,3 +20,22 @@ def solution(priorities, location):
             return answer
 
     return answer  # 몇번째로 인쇄 되는지
+
+'''
+파이썬 데큐 쓰는것도 알아야할듯... 몰라도 되나? enumerate 이거 좋네 리스트랑 인덱스랑 붙어서 동작해야할 때 특히 더 좋은듯 
+
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]   # i 가 인덱스가 되니까 location 하고 똑같아짐 
+    print(queue)
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        print(cur)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+            print(queue)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+'''
