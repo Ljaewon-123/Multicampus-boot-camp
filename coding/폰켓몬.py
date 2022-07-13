7c596922f04d4138dcf707860db56fd3d4f5f9da
Ljaewon-123 <jml6388@naver.com>
2022-07-14 02:26:32 +0900

    best_mon

--- coding/폰켓몬.py
+++ coding/폰켓몬.py
@@ -16,3 +16,25 @@ def solution(nums):   # 무조건 a만큼 뽑는건데 종류가 그 및이 안�
         return cnt
     else:
         return a
+
+'''Best
+
+def solution(nums):
+    answer = 0
+    myList = set(nums)
+    if len(nums)/2 > len(myList):
+        answer = len(myList)
+    else:
+        answer = len(nums)/2
+    return answer
+    
+or
+
+def solution(nums):
+    return min(len(set(nums)), len(nums)//2)
+    
+or
+
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
+'''
